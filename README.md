781599a445dbb0762012e4e3ceac962b99a31a76
김아란 <ahran1224@sju.ac.kr>
2026-09-23 15:48:14 +0900

    chore: 프로젝트 초기 설정

---
+++ README.md
@@ -0,0 +1,10 @@
+venv 활성화
+venv\Scripts\Activate.ps1
+
+서버 켜는 방법
+1. 터미널 명령어
+    flask run
+    flask --debug run
+2. 파이썬 코드
+    app.run(debug=True) (app.py 맨 아래)
+    // python app.py (터미널)
---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# @app.route("/test/<text>")
+# def route_sample(text):
+#     return text
+
+
+@app.route("/")
+def home():
+    return "<h1>메인 페이지</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>소개 페이지</h1>"
+
+@app.route("/test/<text>")
+def route_sample(text):
+    return f"<h1>{text}</h1>"
+
+
+@app.route("/age/<num>")            # 타입 없음
+def age_any(num):
+    return f"<h1>{num}살 - 타입은 {type(num).__name__}</h1>"
+
+@app.route("/age2/<int:num>")       # 정수만
+def age_int(num):
+    return f"<h1>{num}살 - 타입은 {type(num).__name__}</h1>"
+
+@app.route("/age3/<float:num>")     #실수만
+def age_float(num):
+    return f"<h1>{num}살 - 타입은 {type(num).__name__}</h1>"
+
+@ app.route("/hi/<name>")
+def hi_template_render(name):
+    return render_template('hi.html', name=name)
