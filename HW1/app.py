55160ec7ba51b2bfb5dc3c3cbd3bee2fad0053b3
김아란 <ahran1224@sju.ac.kr>
2026-09-23 17:11:44 +0900

    fix: 오류 수정

--- HW1/app.py
+++ HW1/app.py
@@ -4,3 +4,12 @@ app = Flask(__name__)
 @app.route("/")
 def main_template_render():
     return render_template("main.html")
+
+@app.route("/profile")
+def profile_template_render():
+    hobbies = ['산책', '카페가기', 'ott시청']
+    return render_template("profile.html", hobbies=hobbies)
+
+@app.route("/great/<name>")
+def great_name(name):
+    return render_template("great.html", name=name)
